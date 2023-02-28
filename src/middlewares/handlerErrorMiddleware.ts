a1e905f25ac9120d7f4f22e2dbd4fea342a077f1
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2023-02-28 15:21:05 -0300

    build: change output build format

--- src/middlewares/handlerErrorMiddleware.ts
+++ src/middlewares/handlerErrorMiddleware.ts
@@ -6,7 +6,6 @@ const handleError = (
 	err: AppError,
 	req: Request,
 	res: Response,
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	next: NextFunction
 ) => {
 	console.error(err)
